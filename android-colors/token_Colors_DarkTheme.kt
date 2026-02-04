656de81f015432a9c3b922e2190e0de7e97a02a0
zeroheight <tokens-management@zeroheight.com>
2026-02-04 14:59:46 +0500

    Update [Redesign] Asaka Mobile (Copy) from zeroheight

---
+++ android-colors/token_Colors_DarkTheme.kt
@@ -0,0 +1,41 @@
+
+
+
+package ;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object  {
+  val colorBackgroundsPrimary = Color(0xff000000)
+  val colorBackgroundsSecondary = Color(0xff1c1c1e)
+  val colorBackgroundsTertiary = Color(0xff2c2c2e)
+  val colorIconsBrand = Color(0xffff1c2d)
+  val colorIconsInverse = Color(0xff000000)
+  val colorIconsPrimary = Color(0xffffffff)
+  val colorIconsQuaternary = Color(0x29ebebf5)
+  val colorIconsSecondary = Color(0x99ebebf5)
+  val colorIconsTertiary = Color(0x4debebf5)
+  val colorInputPrimary = Color(0xff1c1c1e)
+  val colorMiscAccentsBlue = Color(0xff0091ff)
+  val colorMiscAccentsBrown = Color(0xffb78a66)
+  val colorMiscAccentsCyan = Color(0xff3cd3fe)
+  val colorMiscAccentsGreen = Color(0xff30d158)
+  val colorMiscAccentsIndigo = Color(0xff6d7cff)
+  val colorMiscAccentsMint = Color(0xff00dac3)
+  val colorMiscAccentsOrange = Color(0xffff9230)
+  val colorMiscAccentsPurple = Color(0xffdb34f2)
+  val colorMiscAccentsRed = Color(0xffff2f3e)
+  val colorMiscAccentsTeal = Color(0xff00d2e0)
+  val colorMiscAccentsYellow = Color(0xffffd600)
+  val colorMiscBrandBrand = Color(0xffff1c2d)
+  val colorMiscSeperatorsNonOpaque = Color(0x2bffffff)
+  val colorMiscSeperatorsOpaque = Color(0xff38383a)
+  val colorTabBarNonSelected = Color(0xffadadb3)
+  val colorTabBarSelected = Color(0xffff1c2d)
+  val colorTextsInverse = Color(0xff000000)
+  val colorTextsPrimary = Color(0xffffffff)
+  val colorTextsQuaternary = Color(0x29ebebf5)
+  val colorTextsSecondary = Color(0x99ebebf5)
+  val colorTextsTertiary = Color(0x4debebf5)
+}
